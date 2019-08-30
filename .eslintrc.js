ce906956ba1431bca7d9e7e2d97729f0b04c3e2e
Carlos Levir <carloslevirfs@gmail.com>
2019-08-30 11:13:36 -0300

    Initial commit with style guides

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es6: true
+  },
+  extends: ['airbnb', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': 'off'
+  }
+};
