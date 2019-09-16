c867d9ba0812a40ed6e1e8fdc3d00c5ad618ebc0
Carlos Levir <carloslevirfs@gmail.com>
2019-09-16 13:20:13 -0300

    chore: Create .env.json file

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_BASE_URL } from '../../.env.json';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: API_BASE_URL
 });
 
 export default api;
